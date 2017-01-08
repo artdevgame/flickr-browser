070256b4b7ff6ece7af7614ba5f51fcb7794d159
Mike <me@mikeholloway.co.uk>
2017-01-08 18:26:37 +0000

    Started on some simple tests, needs further development. Styling tweaks for IE.

--- src/components/photo/index.test.js
+++ src/components/photo/index.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Photo from './index';
 
-it('renders without crashing', () => {
+xit('renders without crashing', () => {
 	const div = document.createElement('div');
 	ReactDOM.render(<Photo />, div);
 });
